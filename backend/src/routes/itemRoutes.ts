7b5ad49744e264ec7c5654c6f2bf32a7009f0368
Klaszlo5 <94529898+Klaszlo5@users.noreply.github.com>
2025-01-19 21:20:37 +0100

    Delete backend directory

--- backend/src/routes/itemRoutes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { getItemsController, createOrderController } from '../controllers/itemController';
-
-const router = Router();
-
-router.get('/items', getItemsController);
-router.post('/order', createOrderController);
-
-export default router;
